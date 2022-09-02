4e3febc3001917957aacfb1d252b010e64a3e6ad
Juan Leal <ingjuanleal1989@gmail.com>
2022-09-02 11:46:03 -0500

    token 2 scren

---
+++ src/pages/TokenPage2.js
@@ -0,0 +1,12 @@
+import PageContainer from "../components/layout/PageContainer";
+
+const Token2 = () => {
+  return (
+    <PageContainer>
+      <h3>Token2</h3>
+      <div>Here I can check my token2!</div>
+    </PageContainer>
+  );
+};
+
+export default Token2;
